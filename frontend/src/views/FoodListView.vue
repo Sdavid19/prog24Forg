91a8111ea5152da4d52bab8c59814779c908bc0c
Torpe17 <113579737+Torpe17@users.noreply.github.com>
2023-10-10 23:51:21 +0200

    food-list done

--- frontend/src/views/FoodListView.vue
+++ frontend/src/views/FoodListView.vue
@@ -5,22 +5,31 @@
       :value="foods"
       tableStyle="min-width: 50rem"
       removableSort
-      :globalFilterFields="['name', 'foods.name']"
       filterDisplay="row"
     >
       <template #header>
         <div class="flex justify-content-end">
           <span class="p-input-icon-left">
             <i class="pi pi-search" />
-            <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
+            <InputText v-model="filters['global'].value" placeholder="Keresés" />
           </span>
         </div>
       </template>
-      <Column field="name" header="Név" style="min-width: 12rem"> </Column>
+      <Column field="name" header="Név" style="min-width: 12rem" sortable> </Column>
       <Column field="type" header="Típus" sortable></Column>
-      <Column field="allergies" header="Allergének" sortable></Column>
+      <Column field="allergies" header="Allergének" sortable>
+        <template #body="slotProps">
+          <span v-for="allergy in slotProps.data.allergies">
+            {{}}
+            <img :src="'/src/assets/img/' + allergy[0] + '-mentes.png'" alt="" />
+          </span> </template
+      ></Column>
       <Column field="expirationDate" header="Lejárati dátum" sortable></Column>
-      <Column field="consumption" header="Fogyasztás" sortable></Column>
+      <Column header="Fogyasztás" sortable>
+        <template #body="slotProps">
+          {{ slotProps.data.consumption ? "Helyben fogyasztás" : "Elvitelre" }}
+        </template>
+      </Column>
       <Column field="cousine" header="Konyha típus" sortable></Column>
     </DataTable>
   </div>
@@ -31,6 +40,7 @@ import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import { ref } from "vue";
 import { FilterMatchMode } from "primevue/api";
+import InputText from "primevue/inputtext";
 
 const foods = ref([
   {
@@ -45,6 +55,30 @@ const foods = ref([
     name: "mogyorós párna",
     type: "meleg",
     allergies: ["mogyoró", "laktóz"],
+    expirationDate: "2020-12-12",
+    consumption: false,
+    cousine: "mexikói",
+  },
+  {
+    name: "pizzás csiga",
+    type: "meleg",
+    allergies: [],
+    expirationDate: "2020-12-12",
+    consumption: true,
+    cousine: "olasz",
+  },
+  {
+    name: "barackos párna",
+    type: "meleg",
+    allergies: [],
+    expirationDate: "2020-12-12",
+    consumption: true,
+    cousine: "mexikói",
+  },
+  {
+    name: "túrós táska",
+    type: "meleg",
+    allergies: ["laktóz", "glutén", "tojás", "mogyoró", "hal"],
     expirationDate: "2020-12-12",
     consumption: false,
     cousine: "mexikói",
