f060e275906a463752fc95f79639ead8407aaf62
Torpe17 <113579737+Torpe17@users.noreply.github.com>
2023-10-10 16:35:44 +0200

    Update LoginView.vue

--- frontend/src/views/auth/LoginView.vue
+++ frontend/src/views/auth/LoginView.vue
@@ -1,47 +1,67 @@
 <template>
-    <div class="login-container">
-        <form class="shadow p-5">
-        <h2 class="mb-5 text-center">Bejelentkezés</h2>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email cím</label>
-            <input type="email" class="form-control" id="email">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Jelszó</label>
-            <input type="password" class="form-control" id="password">
-        </div>
-        <div>
-            <p class="text-center text-danger error">Error!</p>
-        </div>
-        <div class="btn-container">
-            <button type="submit" class="btn btn-primary">Bejelentkezés</button>
-        </div>
+  <div class="login-container">
+    <form class="shadow p-5">
+      <h2 class="mb-5 text-center">Bejelentkezés</h2>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email cím</label>
+        <input type="email" class="form-control" id="email" v-model="loginData.email" />
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Jelszó</label>
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          v-model="loginData.password"
+        />
+      </div>
+      <div>
+        <p class="text-center text-danger error">Error!</p>
+      </div>
+      <div class="btn-container">
+        <button type="button" class="btn btn-primary" @click="tryLogin()">
+          Bejelentkezés
+        </button>
+      </div>
     </form>
-    </div>
+  </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 
+const loginData = ref({
+  email: "",
+  password: "",
+});
+
+function tryLogin() {
+  console.log(loginData);
+  if (loginData.value.email && loginData.value.password) {
+    //send request
+  } else {
+    alert("Kérem töltsön ki minden mezőt");
+  }
+}
 </script>
 
 <style lang="css" scoped>
-    .login-container{
-        min-height: calc(100vh - 70px);
-        display: grid;
-        place-items: center;
-    }
-    form{
-        min-width: 300px;
-        width: 40%;
-        max-width: 800px;
-    }
-    .btn-container{
-        margin-top: 30px;
-        display: flex;
-        justify-content: center;
-    }
-    .error{
-        visibility: hidden;
-    }
-
+.login-container {
+  min-height: calc(100vh - 70px);
+  display: grid;
+  place-items: center;
+}
+form {
+  min-width: 300px;
+  width: 40%;
+  max-width: 800px;
+}
+.btn-container {
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+}
+.error {
+  visibility: hidden;
+}
 </style>
