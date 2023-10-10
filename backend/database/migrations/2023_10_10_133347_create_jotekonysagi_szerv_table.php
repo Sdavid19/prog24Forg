f5b9fdb35805a938f7b9e92623e1832c5d47c84f
GurGergely03 <gergely.gurumlai03@gmail.com>
2023-10-10 15:58:48 +0200

    create migrations

--- backend/database/migrations/2023_10_10_133347_create_jotekonysagi_szerv_table.php
+++ backend/database/migrations/2023_10_10_133347_create_jotekonysagi_szerv_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('jotekonysagi_szerv', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('nev', 100);
+            $table->string('email', 100);
+            $table->string('jelszo', 100);
+            $table->double('geolokaciosCim', 50);
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('jotekonysagi_szerv');
     }
 };
