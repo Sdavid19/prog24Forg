a25fe2297040d75255b28199d6363556ade3c6f9
Torpe17 <113579737+Torpe17@users.noreply.github.com>
2023-10-11 01:26:39 +0200

    foodlist filter

--- frontend/src/views/FoodListView.vue
+++ frontend/src/views/FoodListView.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- table -->
   <div class="card mt-3">
     <DataTable
       v-model:filters="filters"
@@ -8,11 +9,25 @@
       filterDisplay="row"
     >
       <template #header>
-        <div class="flex justify-content-end">
-          <span class="p-input-icon-left">
-            <i class="pi pi-search" />
-            <InputText v-model="filters['global'].value" placeholder="Keresés" />
-          </span>
+        <div class="header">
+          <div class="item">
+            <span class="p-input-icon-left">
+              <i class="pi pi-search" />
+              <InputText v-model="filters['global'].value" placeholder="Keresés" />
+            </span>
+          </div>
+          <div class="divider"></div>
+          <div class="item">
+            <label for="distanceSlider" class="px-3 py-2 text-muted">Maximum km:</label>
+            <div class="w-14rem">
+              <InputText v-model.number="filters['distanceInKm'].value" class="w-full" />
+              <Slider
+                v-model="filters['distanceInKm'].value"
+                class="w-full"
+                id="distanceSlider"
+              />
+            </div>
+          </div>
         </div>
       </template>
       <Column field="name" header="Név" style="min-width: 12rem" sortable> </Column>
@@ -31,11 +46,14 @@
         </template>
       </Column>
       <Column field="cousine" header="Konyha típus" sortable></Column>
+      <Column field="distanceInKm" header="Távolság (km)" sortable></Column>
     </DataTable>
   </div>
 </template>
 
 <script setup>
+import Slider from "primevue/slider";
+
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import { ref } from "vue";
@@ -50,6 +68,7 @@ const foods = ref([
     expirationDate: "2022-12-12",
     consumption: false,
     cousine: "olasz",
+    distanceInKm: 5,
   },
   {
     name: "mogyorós párna",
@@ -58,6 +77,7 @@ const foods = ref([
     expirationDate: "2020-12-12",
     consumption: false,
     cousine: "mexikói",
+    distanceInKm: 50,
   },
   {
     name: "pizzás csiga",
@@ -66,6 +86,7 @@ const foods = ref([
     expirationDate: "2020-12-12",
     consumption: true,
     cousine: "olasz",
+    distanceInKm: 6,
   },
   {
     name: "barackos párna",
@@ -74,6 +95,7 @@ const foods = ref([
     expirationDate: "2020-12-12",
     consumption: true,
     cousine: "mexikói",
+    distanceInKm: 9,
   },
   {
     name: "túrós táska",
@@ -82,12 +104,14 @@ const foods = ref([
     expirationDate: "2020-12-12",
     consumption: false,
     cousine: "mexikói",
+    distanceInKm: 17,
   },
 ]);
 
 const filters = ref({
   global: { value: null, matchMode: FilterMatchMode.CONTAINS },
   name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+  distanceInKm: { value: 15, matchMode: FilterMatchMode.LESS_THAN_OR_EQUAL_TO },
 });
 
 console.log(
@@ -95,4 +119,17 @@ console.log(
 );
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+}
+.item {
+  display: flex;
+  flex-shrink: 0;
+}
+.divider {
+  display: flex;
+  flex: 1;
+}
+</style>
