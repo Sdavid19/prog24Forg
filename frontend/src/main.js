bcc8744ef1ee63840dc2de74f0535c4ea13104c7
Torpe17 <113579737+Torpe17@users.noreply.github.com>
2023-10-10 15:26:52 +0200

    basics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { createPinia } from 'pinia';
 
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,6 +12,7 @@ import './assets/css/style.css';
 
 const app = createApp(App);
 
+app.use(createPinia());
 app.use(router);
 
 app.mount('#app');
