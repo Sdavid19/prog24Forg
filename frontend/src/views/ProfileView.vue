a2a31e2b28e7c37343502186995a71688be4f81a
SulyokDavid <75061506+Sdavid19@users.noreply.github.com>
2023-10-10 14:31:25 +0200

    food saver profile start

---
+++ frontend/src/views/ProfileView.vue
@@ -0,0 +1,11 @@
+<template>
+    <FoodSaverProfileComponent/>
+</template>
+
+<script setup>
+    import FoodSaverProfileComponent from '../components/FoodSaverProfileComponent.vue';
+</script>
+
+<style lang="css" scoped>
+
+</style>
