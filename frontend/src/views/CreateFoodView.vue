ed6ae708775ccfada72d8bfc1ad6016004bd0b1f
SulyokDavid <75061506+Sdavid19@users.noreply.github.com>
2023-10-10 18:00:36 +0200

    fix

--- frontend/src/views/CreateFoodView.vue
+++ frontend/src/views/CreateFoodView.vue
@@ -1,7 +1,25 @@
 <template>
   <h1 class="text-center">Étel felajánlás</h1>
+
+  <div class="card">
+    <DataTable
+      :value="foods"
+      paginator
+      :rows="5"
+      :rowsPerPageOptions="[5, 10, 20, 50]"
+      tableStyle="min-width: 50rem"
+    >
+      <Column field="name" header="Name" style="width: 25%"></Column>
+    </DataTable>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+
+const foods = ref([{ name: "asd" }, { name: "asdad" }]);
+</script>
 
 <style lang="css" scoped></style>
