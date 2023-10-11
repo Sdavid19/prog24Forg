26342805731541bb1b6337ad715efe76eb6ec35f
Torpe17 <113579737+Torpe17@users.noreply.github.com>
2023-10-11 04:57:27 +0200

    Update FoodSaverSignupComponent.vue

--- frontend/src/components/FoodSaverSignupComponent.vue
+++ frontend/src/components/FoodSaverSignupComponent.vue
@@ -202,36 +202,50 @@ function tryRegistrate() {
     props.data.email != "" &&
     props.data.password != "" &&
     props.data.geolocation != "" &&
-    moreData.diabetes !== "" &&
-    moreData.allergies != "" &&
-    moreData.intolerances != "" &&
-    moreData.cuisinePreference != "" &&
-    moreData.diet != ""
+    moreData.value.diabetes !== "" &&
+    moreData.value.allergies != "" &&
+    moreData.value.intolerances != "" &&
+    moreData.value.cuisinePreference != "" &&
+    moreData.value.diet != ""
   ) {
+    // let registData = {
+    //   name: props.data.name,
+    //   email: props.data.email,
+    //   password: props.data.password,
+    //   latitude: 10,
+    //   longitude: 10,
+    //   diabetes: 0,
+
+    //   mogyoroAlergia: moreData.allergies.includes("1"),
+    //   halAlergia: moreData.allergies.includes("2"),
+    //   tojasAlergia: moreData.allergies.includes("3"),
+
+    //   laktozErzekenyseg: moreData.intolerances.includes("1"),
+    //   glutenErzekenyseg: moreData.intolerances.includes("2"),
+
+    //   olaszKonyha: moreData.cuisinePreference.includes("1"),
+    //   gorogKonyha: moreData.cuisinePreference.includes("2"),
+    //   amerikaiKonyha: moreData.cuisinePreference.includes("3"),
+    //   mexikoiKonyha: moreData.cuisinePreference.includes("4"),
+    //   magyarKonyha: moreData.cuisinePreference.includes("5"),
+    //   japanKonyha: moreData.cuisinePreference.includes("6"),
+    // };
     let registData = {
       name: props.data.name,
       email: props.data.email,
       password: props.data.password,
       latitude: 10,
       longitude: 10,
-      diabetes: 0,
-
-      mogyoroAlergia: moreData.allergies.includes("1"),
-      halAlergia: moreData.allergies.includes("2"),
-      tojasAlergia: moreData.allergies.includes("3"),
-
-      laktozErzekenyseg: moreData.intolerances.includes("1"),
-      glutenErzekenyseg: moreData.intolerances.includes("2"),
-
-      olaszKonyha: moreData.cuisinePreference.includes("1"),
-      gorogKonyha: moreData.cuisinePreference.includes("2"),
-      amerikaiKonyha: moreData.cuisinePreference.includes("3"),
-      mexikoiKonyha: moreData.cuisinePreference.includes("4"),
-      magyarKonyha: moreData.cuisinePreference.includes("5"),
-      japanKonyha: moreData.cuisinePreference.includes("6"),
+      diabetes: moreData.value.diabetes,
+      allergies: moreData.value.allergies,
+      intolerances: moreData.value.intolerances,
+      cuisinePreference: moreData.value.cuisinePreference,
+      diet: moreData.value.diet,
     };
+    const ready = JSON.stringify(registData);
+    console.log(ready);
     //POST
-    Axios.post("/user-signup", registData.value)
+    Axios.post("/user-signup", ready)
       .then(() => {
         error.value = false;
         console.log("ok");
