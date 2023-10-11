937b4f873d28a0a7019f7a9dcd75699603445ecd
GurGergely03 <gergely.gurumlai03@gmail.com>
2023-10-11 06:37:06 +0200

    foodCreation

--- backend/app/Http/Controllers/etelFelajanlasController.php
+++ backend/app/Http/Controllers/etelFelajanlasController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\etelFelajanlas;
+use Illuminate\Http\foodRequest;
 
 class etelFelajanlasController extends Controller
 {
-    //
+    public function showFood(foodRequest $request){
+        return etelFelajanlas::all();
+    }
+    public function createFood(foodRequest $request){
+        try {
+            $etelFelajanlas = etelFelajanlas::create([
+                'nev' => $request->input('name'),
+                'mennyiseg' => $request->input('mennyiseg'),
+                'mertekegyseg' => $request->input('mertekegyseg'),
+                'pekaru' => $request->input('pekaru'),
+                'tejtermek' => $request->input('tejtermek'),
+                'melegitendo' => $request->input('melegitendo'),
+                'helybenFogyaszthato' => $request->input('helybenFogyaszthato'),
+                'mogyoroAllergia' => $request->input('mogyoroAllergia'),
+                'halAllergia' => $request->input('halAllergia'),
+                'tojasAllergia' => $request->input('tojasAllergia'),
+                'szojaAllergia' => $request->input('szojaAllergia'),
+                'laktozErzekenyeg' => $request->input('laktozErzekenyseg'),
+                'glutenErzekenyseg' => $request->input('glutenErzekenyseg'),
+                'olaszKonyha' => $request->input('olaszKonyha'),
+                'gorogKonyha' => $request->input('gorogKonyha'),
+                'amerikaiKonyha' => $request->input('amerikaiKonyha'),
+                'mexikoiKonyha' => $request->input('mexikoiKonyha'),
+                'magyarKonyha' => $request->input('magyarKonyha'),
+                'japanKonyha' => $request->input('japanKonyha')
+            ])
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
 }
